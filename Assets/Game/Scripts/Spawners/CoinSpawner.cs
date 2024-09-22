4d9390fab7948387907bbac0b1bcedb3a990a21b
Jessica <jduquedeveloper@gmail.com>
2024-09-21 23:17:55 -0300

    Maioria arte substituída / começo prog

--- Assets/Game/Scripts/Spawners/CoinSpawner.cs
+++ Assets/Game/Scripts/Spawners/CoinSpawner.cs
@@ -16,7 +16,7 @@ public class CoinSpawner : NetworkBehaviour
 
     public override void OnNetworkSpawn()
     {
-        if (!IsServer)
+        if (!IsOwner)
         {
             return;
         }
---
+++ Assets/Game/Scripts/Utils/ButtonExtra.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonExtra : MonoBehaviour
+{
+    private Button _thisButton;
+    // _audioManager => AudioManager.I;
+
+    private void Awake()
+    {
+        _thisButton = GetComponent<Button>();
+        _thisButton.onClick.AddListener(MakeSound);
+    }
+
+    private void OnEnable()
+    {
+        _thisButton.interactable = true;
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void MakeSound()
+    {
+        //_audioManager.PlaySfx("buttonclick");
+        _thisButton.interactable = false;
+        StartCoroutine(Reset());
+    }        
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _thisButton.interactable = true;
+    }
+
+}
